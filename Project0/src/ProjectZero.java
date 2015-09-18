59b98f927e8e28ec3b8bb9b420bc895d73d3c389
ethansam <ethansam@edwards-mbp-2.home>
2015-09-17 23:08:20 -0400

    Change.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -5,12 +5,13 @@ public class ProjectZero {
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, input3, result;
+			int input1, input2, result;
 			
 			System.out.print("Please enter two numbers of your exalted choosing: ");
 			input1 = inputSource.nextInt();
 			input2 = inputSource.nextInt();
 			
+			int input3;
 			input 3 = inputSource.nextDouble();
 			result = input1 + input2;
 			System.out.println(result);
