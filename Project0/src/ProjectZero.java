d852db51803c35a155246682c2aa2403c92ec8fd
ethansam <ethansam@edwards-mbp-2.home>
2015-09-17 23:05:52 -0400

    Adding var.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -5,7 +5,7 @@ public class ProjectZero {
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
+			int input1, input2, input3, result;
 			
 			System.out.print("Please enter two numbers of your exalted choosing: ");
 			input1 = inputSource.nextInt();
