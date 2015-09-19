d7c57db97ef9f893011f4e7f2e8bd13ce776cb04
ethansam <ethansam@edwards-mbp-2.home>
2015-09-19 16:45:24 -0400

    Used if/else statements to let the user silently decide whether to enter two integers or two floating point numbers. If the user gives a non-numerical input, they get a message stating that it was neither an integer or floating point number.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -5,21 +5,31 @@ public class ProjectZero {
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
+			int num1,num2;
+			double num3,num4,sum;
+	
 			
-			/*This checks to see if the user inputed an integer or a
-			floating point number.*/ 
+			System.out.println("Enter two numbers.");
 			
-			System.out.print("Enter two numbers.");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
+			/*This checks to see if the user inputed two integers or two
+			floating point numbers.
+			*/ 
+			if(inputSource.hasNextInt()){
+				num1 = inputSource.nextInt();
+				num2 = inputSource.nextInt();
+				sum = num1 + num2;
+				System.out.println(sum);
+			}
+			else if(inputSource.hasNextDouble()){
+				num3 = inputSource.nextDouble();
+				num4 = inputSource.nextDouble();
+				sum = num3 + num4;
+				System.out.println(sum);
+			}
+			else
+				System.out.println("You entered neither an integer or a floating point number.");
 			
-			double input3 = inputSource.nextDouble();
-			double input4 = inputSource.nextDouble();
-			double sum = input3 + input4;
-			result = input1 + input2;
-			System.out.println(result);
-			System.out.println(sum);
+	
 			inputSource.close();
 			
 	
