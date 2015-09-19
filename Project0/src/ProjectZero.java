2f33d35d0a54ebc4dbe309974d1b3677af33f24d
ethansam <ethansam@edwards-mbp-2.home>
2015-09-19 12:17:34 -0400

    Attempting to let the user decide whether to enter integers or floating point numbers.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -7,15 +7,22 @@ public class ProjectZero {
 			Scanner inputSource = new Scanner(System.in);
 			int input1, input2, result;
 			
-			System.out.print("Please enter two numbers of your exalted choosing: ");
+			/*This checks to see if the user inputed an integer or a
+			floating point number.*/ 
+			
+			System.out.print("Enter two numbers.");
 			input1 = inputSource.nextInt();
 			input2 = inputSource.nextInt();
 			
+			double input3 = inputSource.nextDouble();
+			double input4 = inputSource.nextDouble();
+			double sum = input3 + input4;
 			result = input1 + input2;
 			System.out.println(result);
-			
+			System.out.println(sum);
 			inputSource.close();
 			
+	
 	}
 
 }
