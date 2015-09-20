d0305d196adb9888544446e659fe443ccf4fcc6e
ethansam <ethansam@edwards-mbp-2.home>
2015-09-19 22:41:37 -0400

    Attempting to handle the exception of an integer/floating-point combination.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,39 +1,59 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ProjectZero {
 
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int num1,num2;
+			int num1,num2,total;
 			double num3,num4,sum;
-	
+			
 			
 			System.out.println("Enter two numbers.");
 			
 			/*This checks to see if the user inputed two integers or two
-			floating point numbers.
+			floating point numbers using if/else statements. A nested try/catch block was used to 
+			catch the exception where an integer was used 
 			*/ 
-			if(inputSource.hasNextInt()){
-				num1 = inputSource.nextInt();
-				num2 = inputSource.nextInt();
-				sum = num1 + num2;
-				System.out.println(sum);
+			try{
+				try{
+					if(inputSource.hasNextInt()){
+						num1 = inputSource.nextInt();
+						num2 = inputSource.nextInt();
+						total = num1 + num2;
+						System.out.println(total);
+					}
+					else if(inputSource.hasNextDouble()){
+						num3 = inputSource.nextDouble();
+						num4 = inputSource.nextDouble();
+						sum = num3 + num4;
+						System.out.println(sum);
+					}
+					else
+						System.out.println("You entered neither an integer or a floating point number.");
+						
+				}
+				catch(InputMismatchException e){
+					System.out.println("Invalid combination of numbers.");
+				}
 			}
-			else if(inputSource.hasNextDouble()){
-				num3 = inputSource.nextDouble();
-				num4 = inputSource.nextDouble();
-				sum = num3 + num4;
-				System.out.println(sum);
+			
+			/*Catches the exception where a user enters a int first and a decimal second.
+			 */
+			catch(InputMismatchException e2){
+				System.out.println("Invalid combination of numbers.");
 			}
-			else
-				System.out.println("You entered neither an integer or a floating point number.");
 			
-	
-			inputSource.close();
-			
-	
+			inputSource.close();	
 	}
-
+			
+	
 }
+			
+
+
+
+
+
 
